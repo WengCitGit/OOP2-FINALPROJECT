2a4d1ee7c600675ee569d1adb23fdb1d1b773365
Rothesa Arcena <rothesav.arcena@gmail.com>
2026-03-09 19:49:07 +0800

    background
    for character selector and enter username

--- CSIT228G4/core/src/main/java/io/github/PASAN/CharacterSelector.java
+++ CSIT228G4/core/src/main/java/io/github/PASAN/CharacterSelector.java
@@ -7,23 +7,21 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.*;
 import com.badlogic.gdx.utils.viewport.*;
 
-public class CharacterSelector implements Screen
-{
+public class CharacterSelector implements Screen {
     private SpriteBatch batch;
     private BitmapFont font;
     private ShapeRenderer shape;
+    private Texture background;
 
     private String username;
     private String mode;
     private Rectangle[] characters;
-    private String[] characterNames =
-    {
-            "Jollibee", "McDonald", "Colonel Sanders", "Burger King", "Wendy", "Jack in the Box", "Little Caesar",
-            "Chief Khai"
+    private String[] characterNames = {
+            "Jollibee", "McDonald", "Colonel Sanders", "Burger King",
+            "Wendy", "Jack in the Box", "Little Caesar", "Chief Khai"
     };
 
     private Vector3 touch;
-
     private Texture backBtn, backBtnP;
     private Rectangle backBounds;
 
@@ -36,122 +34,113 @@ public class CharacterSelector implements Screen
     private static final float WORLD_WIDTH = 1920;
     private static final float WORLD_HEIGHT = 1080;
 
-    public CharacterSelector(String username, String mode)
-    {
+    public CharacterSelector(String username, String mode) {
         this.username = username;
         this.mode = mode;
 
         batch = new SpriteBatch();
         font = new BitmapFont();
         shape = new ShapeRenderer();
+
         backBtn = new Texture("buttons/back_button.png");
         backBtnP = new Texture("buttons/back_button_pressed.png");
+        background = new Texture("backgrounds/characterselector_background.jpg");
 
         touch = new Vector3();
-
         camera = new OrthographicCamera();
         viewport = new FitViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
-        camera.position.set(WORLD_WIDTH/2, WORLD_HEIGHT/2, 0);
+        camera.position.set(WORLD_WIDTH / 2, WORLD_HEIGHT / 2, 0);
 
         characters = new Rectangle[8];
-
-        for(int i = 0; i < 8; i++)
-        {
+        for (int i = 0; i < 8; i++) {
             int row = i / 4;
             int col = i % 4;
-
-            characters[i] = new Rectangle(400 + col * 300, 550 - row * 300, 200, 200);
+            characters[i] = new Rectangle(400 + col * 300, 600 - row * 350, 200, 200);
         }
 
-        backBounds = new Rectangle(400, 100, 220, 100);
+        backBounds = new Rectangle(50, 50, 300, 100);
     }
 
     @Override
-    public void render(float delta)
-    {
-        Gdx.gl.glClearColor(0,0,0,1);
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         camera.update();
-
-        touch.set(Gdx.input.getX(), Gdx.input.getY(),0);
+        touch.set(Gdx.input.getX(), Gdx.input.getY(), 0);
         viewport.unproject(touch);
-        boolean pressingBack = Gdx.input.isTouched() && backBounds.contains(touch.x, touch.y);
-
-        shape.setProjectionMatrix(camera.combined);
-        shape.begin(ShapeRenderer.ShapeType.Line);
-
-        for(int i = 0; i < characters.length; i++)
-        {
-            Rectangle r = characters[i];
-            shape.rect(r.x, r.y, r.width, r.height);
-        }
-
-        shape.end();
 
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
 
-        //Name Placeholders
-        for(int i = 0; i < characters.length; i++)
-        {
-            Rectangle r = characters[i];
-            GlyphLayout layout = new GlyphLayout(font, characterNames[i]);
-            float textX = r.x + (r.width - layout.width)/2;
-            float textY = r.y + (r.height + layout.height)/2;
-            font.draw(batch, layout, textX, textY);
+        batch.draw(background, 0, 0, WORLD_WIDTH, WORLD_HEIGHT);
+
+        boolean isTouchingBack = Gdx.input.isTouched() && backBounds.contains(touch.x, touch.y);
+        if (isTouchingBack) {
+            batch.draw(backBtnP, backBounds.x, backBounds.y, backBounds.width, backBounds.height);
+        } else {
+            batch.draw(backBtn, backBounds.x, backBounds.y, backBounds.width, backBounds.height);
         }
 
-        if(pressingBack) batch.draw(backBtnP, backBounds.x, backBounds.y, backBounds.width, backBounds.height);
-        else batch.draw(backBtn, backBounds.x, backBounds.y, backBounds.width, backBounds.height);
+        font.getData().setScale(3);
+        font.draw(batch, "PLAYER: " + username, 50, 1030);
+        font.draw(batch, "MODE: " + mode, 50, 970);
+        //font.draw(batch, "SELECT CHARACTER", 720, 930);
 
-        font.draw(batch,"PLAYER: "+username, 50, 1000);
-        font.draw(batch, "MODE: "+mode, 50, 950);
-        font.draw(batch,"SELECT CHARACTER", 850, 900);
+        font.getData().setScale(2);
+        for (int i = 0; i < characters.length; i++) {
+            Rectangle r = characters[i];
+            GlyphLayout layout = new GlyphLayout(font, characterNames[i]);
+            font.draw(batch, layout, r.x + (r.width - layout.width) / 2, r.y + r.height + 40);
+        }
 
         batch.end();
+
+        shape.setProjectionMatrix(camera.combined);
+        shape.begin(ShapeRenderer.ShapeType.Line);
+        shape.setColor(Color.WHITE);
+        for (Rectangle r : characters) {
+            shape.rect(r.x, r.y, r.width, r.height);
+        }
+        shape.end();
 
         handleInput();
     }
 
-    private void handleInput()
-    {
-        if(Gdx.input.justTouched())
-        {
-            if(backBounds.contains(touch.x, touch.y)) backPressed = true;
-            for(int i = 0; i < characters.length; i++)
-            {
-                if(characters[i].contains(touch.x, touch.y)) characterPressed = i;
+    private void handleInput() {
+        if (Gdx.input.justTouched()) {
+            if (backBounds.contains(touch.x, touch.y)) backPressed = true;
+            for (int i = 0; i < characters.length; i++) {
+                if (characters[i].contains(touch.x, touch.y)) characterPressed = i;
             }
         }
 
-        if(!Gdx.input.isTouched())
-        {
-            if(backPressed && backBounds.contains(touch.x, touch.y)) ((Main) Gdx.app.getApplicationListener()).setScreen(new UsernameScreen(mode));
-            if(characterPressed != -1 && characters[characterPressed].contains(touch.x, touch.y))
-            {
-                System.out.println("Character "+characterPressed+" selected");
-                //Start game here
+        if (!Gdx.input.isTouched()) {
+            if (backPressed && backBounds.contains(touch.x, touch.y)) {
+                ((Main) Gdx.app.getApplicationListener()).setScreen(new UsernameScreen(mode));
             }
-
+            if (characterPressed != -1 && characters[characterPressed].contains(touch.x, touch.y)) {
+                System.out.println("Selected: " + characterNames[characterPressed]);
+            }
             backPressed = false;
             characterPressed = -1;
         }
     }
 
-    @Override public void resize(int width, int height)
-    {
+    @Override
+    public void resize(int width, int height) {
         viewport.update(width, height);
     }
 
-    @Override public void show(){};
-    @Override public void pause(){};
-    @Override public void resume(){};
-    @Override public void hide(){};
+    @Override public void show() {}
+    @Override public void pause() {}
+    @Override public void resume() {}
+    @Override public void hide() {}
 
-    @Override public void dispose()
-    {
+    @Override
+    public void dispose() {
         batch.dispose();
+        background.dispose();
         font.dispose();
         shape.dispose();
         backBtn.dispose();
--- CSIT228G4/core/src/main/java/io/github/PASAN/UsernameScreen.java
+++ CSIT228G4/core/src/main/java/io/github/PASAN/UsernameScreen.java
@@ -7,8 +7,7 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.*;
 import com.badlogic.gdx.utils.viewport.*;
 
-public class UsernameScreen implements Screen
-{
+public class UsernameScreen implements Screen {
 
     private SpriteBatch batch;
     private BitmapFont font;
@@ -18,7 +17,6 @@ public class UsernameScreen implements Screen
     private String gameMode;
 
     private Rectangle enterBounds;
-    private Rectangle cancelBounds;
     private Rectangle textFieldBounds;
     private Rectangle backBounds;
 
@@ -28,6 +26,7 @@ public class UsernameScreen implements Screen
     private Viewport viewport;
 
     private Texture backBtn, backBtnP;
+    private Texture background;
 
     private static final float WORLD_WIDTH = 1920;
     private static final float WORLD_HEIGHT = 1080;
@@ -36,9 +35,7 @@ public class UsernameScreen implements Screen
     private boolean showCursor = true;
     private boolean backPressed = false;
 
-    public UsernameScreen(String mode)
-    {
-
+    public UsernameScreen(String mode) {
         this.gameMode = mode;
 
         batch = new SpriteBatch();
@@ -47,6 +44,7 @@ public class UsernameScreen implements Screen
 
         backBtn = new Texture("buttons/back_button.png");
         backBtnP = new Texture("buttons/back_button_pressed.png");
+        background = new Texture("backgrounds/usernameScreen_background.jpg");
 
         touchPoint = new Vector3();
 
@@ -56,150 +54,126 @@ public class UsernameScreen implements Screen
 
         float centerX = WORLD_WIDTH / 2f;
 
-        textFieldBounds = new Rectangle(centerX - 300, 560, 600, 80);
-
-        enterBounds = new Rectangle(centerX + 40, 350, 220, 100);
-        cancelBounds = new Rectangle(centerX - 260, 350, 220, 100);
-
-        backBounds = new Rectangle(centerX - 260, 350, 220, 100);
+        textFieldBounds = new Rectangle(centerX - 320, 650, 620, 80);
+        enterBounds = new Rectangle(centerX + 40, 450, 250, 100);
+        backBounds = new Rectangle(centerX - 300, 450, 250, 100);
     }
 
     @Override
-    public void show()
-    {
-
-        Gdx.input.setInputProcessor(new InputAdapter()
-        {
-
+    public void show() {
+        Gdx.input.setInputProcessor(new InputAdapter() {
             @Override
-            public boolean keyTyped(char character)
-            {
-
-                if(character == '\b' && username.length() > 0) username = username.substring(0, username.length()-1);
-                //else if(character == '\r') confirm();
-                else if(Character.isLetterOrDigit(character) && username.length() < 12) username += character;
-
+            public boolean keyTyped(char character) {
+                if (character == '\b' && username.length() > 0) {
+                    username = username.substring(0, username.length() - 1);
+                } else if (Character.isLetterOrDigit(character) && username.length() < 12) {
+                    username += character;
+                }
                 return true;
             }
 
             @Override
-            public boolean keyDown(int keycode)
-            {
-
-                if(keycode == Input.Keys.ESCAPE) cancel();
-                if(keycode == Input.Keys.ENTER) confirm();
+            public boolean keyDown(int keycode) {
+                if (keycode == Input.Keys.ESCAPE) cancel();
+                if (keycode == Input.Keys.ENTER) confirm();
                 return true;
             }
         });
     }
 
     @Override
-    public void render(float delta)
-    {
+    public void render(float delta) {
         camera.update();
 
         cursorTimer += delta;
-        if(cursorTimer > 0.5f)
-        {
+        if (cursorTimer > 0.5f) {
             showCursor = !showCursor;
             cursorTimer = 0;
         }
 
-        Gdx.gl.glClearColor(0,0,0,1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         touchPoint.set(Gdx.input.getX(), Gdx.input.getY(), 0);
         viewport.unproject(touchPoint);
-        boolean pressingBack = Gdx.input.isTouched() && backBounds.contains(touchPoint.x, touchPoint.y);
-        if(Gdx.input.justTouched() && backBounds.contains(touchPoint.x, touchPoint.y)) backPressed = true;
-        if(backPressed && !Gdx.input.isTouched())
-        {
-            if(backBounds.contains(touchPoint.x, touchPoint.y)) cancel();
-            backPressed = false;
-        }
 
-        // ---------- DRAW SHAPES ----------
-        shape.setProjectionMatrix(camera.combined);
-        shape.begin(ShapeRenderer.ShapeType.Line);
+        boolean isTouchingBack = Gdx.input.isTouched() && backBounds.contains(touchPoint.x, touchPoint.y);
 
-        //Cancel PlaceHolder
-        shape.rect(textFieldBounds.x, textFieldBounds.y, textFieldBounds.width, textFieldBounds.height);
-        shape.rect(enterBounds.x, enterBounds.y, enterBounds.width, enterBounds.height);
-        //shape.rect(cancelBounds.x, cancelBounds.y, cancelBounds.width, cancelBounds.height);
-
-        shape.end();
-
-        // ---------- DRAW UI ----------
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
 
-        // Back button
-        if(pressingBack) batch.draw(backBtnP, backBounds.x, backBounds.y, backBounds.width, backBounds.height);
-        else batch.draw(backBtn, backBounds.x, backBounds.y, backBounds.width, backBounds.height);
+        batch.draw(background, 0, 0, WORLD_WIDTH, WORLD_HEIGHT);
 
-        font.draw(batch,"ENTER USERNAME",820,700);
+        if (isTouchingBack) {
+            batch.draw(backBtnP, backBounds.x, backBounds.y, backBounds.width, backBounds.height);
+        } else {
+            batch.draw(backBtn, backBounds.x, backBounds.y, backBounds.width, backBounds.height);
+        }
+
+        font.getData().setScale(3);
+        //font.draw(batch, "ENTER USERNAME", 820, 750);
 
         String displayName = username;
+        if (showCursor) displayName += "|";
+        font.draw(batch, displayName, textFieldBounds.x + 20, textFieldBounds.y + 60);
 
-        if(showCursor) displayName += "|";
-
-        font.draw(batch,displayName, textFieldBounds.x + 20, textFieldBounds.y + 50);
-
-        font.draw(batch,"ENTER", enterBounds.x + 70, enterBounds.y + 60);
-        //font.draw(batch,"CANCEL", cancelBounds.x + 60, cancelBounds.y + 60);
+        font.draw(batch, "ENTER", enterBounds.x + 45, enterBounds.y + 70);
 
         batch.end();
+
+        shape.setProjectionMatrix(camera.combined);
+        shape.begin(ShapeRenderer.ShapeType.Line);
+        shape.setColor(Color.WHITE);
+        shape.rect(textFieldBounds.x, textFieldBounds.y, textFieldBounds.width, textFieldBounds.height);
+        shape.rect(enterBounds.x, enterBounds.y, enterBounds.width, enterBounds.height);
+        shape.end();
 
         handleMouse();
     }
 
-    private void handleMouse()
-    {
+    private void handleMouse() {
+        if (Gdx.input.justTouched()) {
+            if (enterBounds.contains(touchPoint.x, touchPoint.y)) confirm();
+            if (backBounds.contains(touchPoint.x, touchPoint.y)) backPressed = true;
+        }
 
-        if(Gdx.input.justTouched()){
-            if(enterBounds.contains(touchPoint.x, touchPoint.y)) confirm();
-            //if(cancelBounds.contains(touchPoint.x, touchPoint.y)) cancel();
-            //if(backBounds.contains(touchPoint.x, touchPoint.y)) cancel();
+        if (backPressed && !Gdx.input.isTouched()) {
+            if (backBounds.contains(touchPoint.x, touchPoint.y)) cancel();
+            backPressed = false;
         }
     }
 
-    private void confirm()
-    {
-        if(username.length() == 0) return;
-        if(gameMode == null || gameMode.isEmpty()) gameMode = "DEFAULT";
+    private void confirm() {
+        if (username.length() == 0) return;
+        if (gameMode == null || gameMode.isEmpty()) gameMode = "DEFAULT";
 
-        // Wrap in try-catch to prevent crashes
         try {
-            CharacterSelector selector = new CharacterSelector(username, gameMode);
-            ((Main) Gdx.app.getApplicationListener()).setScreen(selector);
-        } catch(Exception e) {
+            ((Main) Gdx.app.getApplicationListener()).setScreen(new CharacterSelector(username, gameMode));
+        } catch (Exception e) {
             e.printStackTrace();
-            // fallback or log error
         }
     }
 
-    private void cancel()
-    {
+    private void cancel() {
         ((Main) Gdx.app.getApplicationListener()).setScreen(new GameModeScreen());
     }
 
     @Override
-    public void resize(int width, int height)
-    {
-        viewport.update(width,height);
+    public void resize(int width, int height) {
+        viewport.update(width, height);
     }
 
-    @Override public void pause(){}
-    @Override public void resume(){}
-    @Override public void hide(){}
+    @Override public void pause() {}
+    @Override public void resume() {}
+    @Override public void hide() {}
 
     @Override
-    public void dispose()
-    {
+    public void dispose() {
         batch.dispose();
         font.dispose();
         shape.dispose();
         backBtn.dispose();
         backBtnP.dispose();
+        background.dispose();
     }
 }
